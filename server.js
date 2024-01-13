e779f58fb32b4c1a932e0b0549b3644cf11b88b9
pumpkinstuf <156490378+pumpkinstuf@users.noreply.github.com>
2024-01-13 08:06:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ CLIENTS = [];
 const wss = new WebSocket.Server({ port: 8000 });
 wss.on('connection', (ws) => {
   CLIENTS.push(ws);
-  console.log(CLIENTS);
   ws.on('message', (data) => {
-    console.log(data)
+    //console.log(data)
     wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
           client.send(data);
